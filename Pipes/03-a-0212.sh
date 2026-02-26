827c6442e667d2bc37a15bd6567d62d7c476ddbd
Kalata <153134271+7Kaloyan7Markov7@users.noreply.github.com>
2026-02-26 18:06:12 +0200

    Update 03-a-0212.sh

--- Pipes/03-a-0212.sh
+++ Pipes/03-a-0212.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat /etc/passwd | tr -s ',/' '::' | cut -d ':' -f5,9 | tr ':' ' '
+cat /etc/passwd | tr -s ',/' ':' | cut -d ':' -f5,9 | tr ':' ' '
 #cat /etc/passwd | tr -s ',' ':' | cut -d ':' -f 5,7 | tr -s ':' ' '
