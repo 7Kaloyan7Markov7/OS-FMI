fb0ac1285e3a00b6b915995419a7fd356dc6e4bb
Kalata <153134271+7Kaloyan7Markov7@users.noreply.github.com>
2026-02-27 00:32:03 +0200

    Update 03-a-5000.sh

--- Pipes/03-a-5000.sh
+++ Pipes/03-a-5000.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cat /etc/passwd | grep $(whoami)
-cat /etc/passwd |grep -B 2 $(whoami)
-cat /etc/passwd |grep -B 2 -A 3 $(whoami)
+cat /etc/passwd | grep -B 2 $(whoami)
+cat /etc/passwd | grep -B 2 -A 3 $(whoami)
 cat /etc/passwd | grep -B 3 $(whoami)
