a3fb56527894596dd9401ccddc0adb706e219812
Kalata <153134271+7Kaloyan7Markov7@users.noreply.github.com>
2026-03-01 03:30:10 +0200

    Update 03-a-6000.sh

--- Pipes/03-a-6000.sh
+++ Pipes/03-a-6000.sh
@@ -2,3 +2,13 @@
 cp /srv/fmi-os/exercises/data/emp.data ~/
 
 cat emp.data | awk 'END {print NR}'
+cat emp.data | awk -F' ' ' {print $NF}'
+cat emp.data | awk -F' ' ' END {print $NF}'
+cat emp.data | awk -F' ' ' $NF  > 4 {print $0}'
+cat emp.data | awk -F' ' ' length(NF) > 4 {print $0}'
+cat emp.data | awk '{total += NF} END {print total}'
+cat emp.data | awk '/Beth/'
+cat emp.data | awk ' length($3) > max { max = length($3); line = $0} END {print line}'
+cat emp.data | awk ' NF > 0 '
+cat emp.data | awk ' $NF > 17 '
+#TO DO
