9084684b4b0f3a5edf55353d1a450fe7827a401a
Kalata <153134271+7Kaloyan7Markov7@users.noreply.github.com>
2026-05-04 17:58:50 +0300

    Fix find command syntax for file type option

--- Text Book/2022-SE-01.sh
+++ Text Book/2022-SE-01.sh
@@ -10,8 +10,8 @@ home_dir=$(grep -e "\b$(whoami)\b" /etc/passwd | cut -d ':' -f 6)
 files_home_dir=$(mktemp)
 all_files=$(mktemp)
 
-find ${home_dir} -mindepth 1 -maxdepth 1 -user $(whoami) -type f | sort > ${files_home_dir}
-find ${home_dir} -user $(whoami) -type f | sort > ${all_files}
+find ${home_dir} -mindepth 1 -maxdepth 1 -user $(whoami) -type -f | sort > ${files_home_dir}
+find ${home_dir} -user $(whoami) -type -f | sort > ${all_files}
 
 comm -23 ${files_home_dir} ${all_files} | awk '{print $1}' | xargs chmod 664 > /dev/null 2>&1
 
